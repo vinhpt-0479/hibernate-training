2ae4134b773ce080afc340c257a7758000281aab
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-07-17 15:30:17 +0700

    feat(chapter 3-5): hibernate configure with Spring MVC JPA

---
+++ hibernate-configure-with-spring/src/main/java/com/example/hibernateconfigurewithspring/HibernateConfigureWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernateconfigurewithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateConfigureWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateConfigureWithSpringApplication.class, args);
+    }
+
+}
